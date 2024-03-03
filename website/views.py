091e475030e3f2ad1412afd9360224cfd307f1bb
Zakaria Messous <bosszaki2001@gmail.com>
2024-03-03 18:45:34 +0100

    profile

--- website/views.py
+++ website/views.py
@@ -7,4 +7,14 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("base.html")
+
+@views.route('/profile')
+def profile():
+    # Your profile page logic here
+    return render_template('profile.html')
+
+@views.route('/dashboard')
+def dashboard():
+    # Your profile page logic here
+    return render_template('dashboard.html')
